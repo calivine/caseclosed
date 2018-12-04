6bf621babb9075ebc4556b0928f7119595278751
Alex Caloggero <a.calog@protonmail.com>
2018-12-04 18:28:58 -0500

    Updated seeders.

--- database/seeds/IncidentsTableSeeder.php
+++ database/seeds/IncidentsTableSeeder.php
@@ -16,17 +16,17 @@ class IncidentsTableSeeder extends Seeder
     public function run()
     {
         $incidents = [
-            [9,2,null,1978,'Raped and strangled.','https://abcnews.go.com/WNT/story?id=129709&page=1'],
-            [10,3,null,2012,null,null],
-            [11,4,'1969-01-07',1969,'Blunt force trauma','https://nypost.com/2018/11/20/dna-evidence-leads-to-closure-in-1969-cold-case-murder-da/'],
-            [12,5,'1973-02-13',1973,'Strangulation ','https://abcnews.go.com/US/genetic-genealogy-leads-arrest-1973-cold-case-murder/story?id=59343378#'],
-            [13,6,null,1974,'shot during armed robbery',null],
-            [14,7,'1974-10-13',1974,'satanic ritual killing?','https://www.mercurynews.com/2018/06/28/sheriff-suspect-in-infamous-1974-stanford-chapel-murder-shoots-self-as-detectives-close-in/'],
-            [15,8,'1975-09-11',1975,'Shot during burgalry.','https://www.sacbee.com/latest-news/article209851009.html'],
-            [16,9,'1976-12-26',1976,'stabbed to death and sexually assaulted.','stabbed to death and sexually assaulted.'],
-            [17,10,'1977-12-06',1977,'raped and stabbed.',null],
-            [18,11,'1978-02-02',1978,'Shot.',null],
-            [19,12,'1978-02-02',1978,'Shot.',null]
+            [1,1,null,1978,'Raped and strangled.','https://abcnews.go.com/WNT/story?id=129709&page=1'],
+            [2,2,null,2012,null,null],
+            [3,3,'1969-01-07',1969,'Blunt force trauma','https://nypost.com/2018/11/20/dna-evidence-leads-to-closure-in-1969-cold-case-murder-da/'],
+            [4,4,'1973-02-13',1973,'Strangulation ','https://abcnews.go.com/US/genetic-genealogy-leads-arrest-1973-cold-case-murder/story?id=59343378#'],
+            [5,5,null,1974,'shot during armed robbery',null],
+            [6,6,'1974-10-13',1974,'satanic ritual killing?','https://www.mercurynews.com/2018/06/28/sheriff-suspect-in-infamous-1974-stanford-chapel-murder-shoots-self-as-detectives-close-in/'],
+            [7,7,'1975-09-11',1975,'Shot during burgalry.','https://www.sacbee.com/latest-news/article209851009.html'],
+            [8,8,'1976-12-26',1976,'stabbed to death and sexually assaulted.','stabbed to death and sexually assaulted.'],
+            [9,9,'1977-12-06',1977,'raped and stabbed.',null],
+            [10,10,'1978-02-02',1978,'Shot.',null],
+            [11,11,'1978-02-02',1978,'Shot.',null]
         ];
 
         foreach ($incidents as $key => $incidentData) {
--- database/seeds/PerpetratorsTableSeeder.php
+++ database/seeds/PerpetratorsTableSeeder.php
@@ -15,17 +15,17 @@ class PerpetratorsTableSeeder extends Seeder
     public function run()
     {
         $perps = [
-            ['Norman','Whitehorn',9,null,null,null],
-            ['Geovanni','Borjas',10,null,null,null],
-            ['Michael','Sumpter',11,null,null,null],
-            ['John','Getreu',12,1944,'2018-11-20',null],
-            ['Johnnie','Miller',13,null,null,null],
-            ['Stephen','Crawford',14,1946,null,'Suicide upon confrontation.'],
-            ['Joseph','DeAngelo',15,1945,'2018-04-24','Golden State Killer'],
-            ['Ricky','Harnish',16,1955,'2008-02-01',null],
-            ['Kenneth','Matthews',17,null,'2018-04-24',null],
-            ['Joseph','DeAngelo',18,1945,'2018-04-24','Golden State Killer'],
-            ['Joseph','DeAngelo',19,1945,'2018-04-24','Golden State Killer']
+            ['Norman','Whitehorn',1,null,null,null],
+            ['Geovanni','Borjas',2,null,null,null],
+            ['Michael','Sumpter',3,null,null,null],
+            ['John','Getreu',4,1944,'2018-11-20',null],
+            ['Johnnie','Miller',5,null,null,null],
+            ['Stephen','Crawford',6,1946,null,'Suicide upon confrontation.'],
+            ['Joseph','DeAngelo',7,1945,'2018-04-24','Golden State Killer'],
+            ['Ricky','Harnish',8,1955,'2008-02-01',null],
+            ['Kenneth','Matthews',9,null,'2018-04-24',null],
+            ['Joseph','DeAngelo',10,1945,'2018-04-24','Golden State Killer'],
+            ['Joseph','DeAngelo',11,1945,'2018-04-24','Golden State Killer']
         ];
 
         foreach ($perps as $key => $perpData) {
