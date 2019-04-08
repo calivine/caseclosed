843880ef99489fc40179e0a71dcfc1628a501ef8
Alex Caloggero <a.calog@protonmail.com>
2019-04-08 19:54:25 -0400

    Style updates in progress; Fixed bug when pulling images

--- database/seeds/ImagesTableSeeder.php
+++ database/seeds/ImagesTableSeeder.php
@@ -18,7 +18,7 @@ class ImagesTableSeeder extends Seeder
             ['https://c.o0bg.com/rf/image_371w/Boston/2011-2020/2018/11/20/BostonGlobe.com/Spotlight/Images/IMG_1156.jpg','victim','Jane Sanders Britton',1],
             ['https://cbsnews2.cbsistatic.com/hub/i/r/2018/11/20/b93305d9-994a-4953-a239-7f3e381d7213/resize/620x/39a21727465ac088089d9c116d7dbcaf/sumpter2.jpg','perpetrator',null,1],
             ['https://c.o0bg.com/rf/image_835w/Boston/2011-2020/2017/04/03/BostonGlobe.com/Spotlight/Images/FullSizeRender%20(2).jpg','other',null,1],
-            ['https://www.twincities.com/wp-content/uploads/2015/11/20100218__100219rutchick.jpg?w=500','victim',null,2]
+            ['https://www.twincities.com/wp-content/uploads/2015/11/20100218__100219rutchick.jpg?w=500','victim','Ellen Faith Rutchick',1]
 
         ];
 
--- resources/views/case/profile.blade.php
+++ resources/views/case/profile.blade.php
@@ -1,82 +1,74 @@
 @extends('layouts.master')
 
 @section('content')
-    <div class='row'>
-        <div class='col-3-4'>
-            <section>
-                <article>
-                    <header>
-                        <h2>
-                            {{ $victim->first_name }} {{ $victim->middle_name }} {{ $victim->last_name }}
-                        </h2>
-                        @if($victImage != null)
-                                <img src='{{ $victImage->url }}'
-                                     alt='Victim profile picture'
-                                     width='400px'
-                                     height='450px'>
-                        @endif
-                    </header>
-                    <p>Born: {{ $victim->date_of_birth->format('j F, Y') }}</p>
-                    <p>
-                        {{ $victim->description }}
-                    </p>
-                </article>
-                <article>
-                    <header>
-                        <h2>
-                            {{ $victim->perpetrator->first_name }} {{ $victim->perpetrator->last_name }}
-                        </h2>
-                    </header>
-                    <p>
-                        {{ $victim->perpetrator->description }}
-                    </p>
-                </article>
-            </section>
-        </div>
-        <div class='col-1-4'>
-            <aside>
-                <h2>
-                    Details
-                </h2>
-                <ul>
-                    <li>
-                        Crime commited by: {{ $victim->perpetrator->first_name }} {{ $victim->perpetrator->last_name }}
-                    </li>
-                    @if($perpImage != null)
-                            <img src='{{ $perpImage->url }}'
-                                 alt='Perpetrator picture' width='350px'>
+    <div class='profile'>
+        <section>
+            <article>
+                <header>
+                    <h2>
+                        {{ $victim->first_name }} {{ $victim->middle_name }} {{ $victim->last_name }}
+                    </h2>
+                    @if($victImage != null)
+                        <img src='{{ $victImage->url }}'
+                             alt='Victim profile picture'
+                             width='400px'
+                             height='450px'>
                     @endif
-                    <h4>
-                        Victim(s)
-                    </h4>
-                    @foreach($victim->perpetrator->victims as $victimName)
-                        <li>
-                            @if(Request::is(substr('/profile/' . $victimName->last_name, 1)))
+                </header>
+                <p>Born: {{ $victim->date_of_birth->format('j F, Y') }}</p>
+                <p>
+                    {{ $victim->description }}
+                </p>
+            </article>
+            <article>
+                <header>
+                    <h2>
+                        {{ $victim->perpetrator->first_name }} {{ $victim->perpetrator->last_name }}
+                    </h2>
+                </header>
+                <p>
+                    {{ $victim->perpetrator->description }}
+                </p>
+            </article>
+        </section>
+        <aside>
+            <h2>
+                Details
+            </h2>
+            <ul>
+                <li>
+                    Crime commited by: {{ $victim->perpetrator->first_name }} {{ $victim->perpetrator->last_name }}
+                </li>
+                @if($perpImage != null)
+                    <img src='{{ $perpImage->url }}'
+                         alt='Perpetrator picture' width='350px'>
+                @endif
+                <h4>
+                    Victim(s)
+                </h4>
+                @foreach($victim->perpetrator->victims as $victimName)
+                    <li>
+                        @if(Request::is(substr('/profile/' . $victimName->last_name, 1)))
+                            {{ $victimName->first_name }} {{ $victimName->last_name }}
+                        @else
+                            <a href='{{'/profile/' . $victimName->last_name }}'>
                                 {{ $victimName->first_name }} {{ $victimName->last_name }}
-                            @else
-                                <a href='{{'/profile/' . $victimName->last_name }}'>
-                                    {{ $victimName->first_name }} {{ $victimName->last_name }}
-                                </a>
-                            @endif
-                        </li>
-                    @endforeach
-                    <li>
-                        Incident date: {{ $victim->incident_date->format('j F, Y') }}
+                            </a>
+                        @endif
                     </li>
-                    <li>
-                        Cause of death: {{ $victim->cause_of_death }}
-                    </li>
-                </ul>
-            </aside>
-        </div>
-    </div>
-    <div class='row'>
-        <div class='col-3-4'>
-            @foreach($sources as $source)
-                <a href='{{ $source->url }}' target='_blank'>
-                    {{ $source->url }}
-                </a>
-            @endforeach
-        </div>
+                @endforeach
+                <li>
+                    Incident date: {{ $victim->incident_date->format('j F, Y') }}
+                </li>
+                <li>
+                    Cause of death: {{ $victim->cause_of_death }}
+                </li>
+            </ul>
+        </aside>
+        @foreach($sources as $source)
+            <a href='{{ $source->url }}' target='_blank'>
+                {{ $source->url }}
+            </a>
+        @endforeach
     </div>
 @endsection
