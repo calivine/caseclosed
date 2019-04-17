28dba169f027200e857cbea36beb6ae8d6921f92
Alex Caloggero <a.calog@protonmail.com>
2019-04-17 02:19:30 -0400

    Updated about page

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -7,6 +7,9 @@
         <p>If you have any questions, comments, corrections, or find want to report bugs/errors. Leave us a message
             <a href='{{ '/messages/create' }}'>here</a>. If you have any suggestions for additions to the database.
             You can also submit them here until the <a href='{{ url('https://forums.pitchforked.net') }}'>forums</a> are complete.</p>
+        <p>Want to support the project? <a href='{{ url('https://www.paypal.me/bytlg') }}'>Donate</a> here. </p>
+        
+
     </section>
 
 @endsection
