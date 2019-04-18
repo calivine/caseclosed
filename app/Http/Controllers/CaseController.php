fac54b385a3e594fd2a580ed53f0d740d6336f06
Alex Caloggero <a.calog@protonmail.com>
2019-04-18 00:13:53 -0400

    Updated about page

--- app/Http/Controllers/CaseController.php
+++ app/Http/Controllers/CaseController.php
@@ -4,7 +4,6 @@ namespace App\Http\Controllers;
 
 use App\Message;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\DB;
 use App\Perpetrator;
 use App\Victim;
 use App\Actions\Victim\GetVictim;
--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -5,10 +5,10 @@
         <div class='aboutContent'>
             <p>Case Closed is creating an archive of murders and missing persons cases solved primarily through DNA
                 evidence</p>
-            <p>If you have any questions, comments, corrections, or find want to report bugs/errors. Leave us a message
-                <a href='{{ '/messages/create' }}'>here</a>. If you have any suggestions for additions to the database.
+            <p>If you have any questions, comments, corrections, or find want to report bugs/errors. Leave us a
+                <a href='{{ '/messages/create' }}'>message</a>. If you have any suggestions for additions to the database.
                 You can also submit them here until the <a href='{{ url('https://forums.pitchforked.net') }}'>forums</a> are complete.</p>
-            <p>Want to support the project? <a href='{{ url('https://www.paypal.me/bytlg') }}'>Donate</a> here. </p>
+            <p>Want to support the project? <a href='{{ url('https://www.paypal.me/bytlg') }}'>Donate.</a></p>
             <p>Case Closed is an open source <a href='{{ url('https://github.com/calivine/caseclosed') }}'>project.</a></p>
         </div>
     </section>
