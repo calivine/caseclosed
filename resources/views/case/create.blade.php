e65d09768f7c27fbcd48bea5aaca331d1a41cc8a
Alex Caloggero <a.calog@protonmail.com>
2019-04-25 12:19:30 -0400

    Updated form layout

--- resources/views/case/create.blade.php
+++ resources/views/case/create.blade.php
@@ -1,34 +1,30 @@
 @extends('layouts.master')
 
 @section('content')
-    <div class='row'>
-        <div class='col-1-3'>
-            <form class='add-input' method='POST' action='{{ '/create' }}'>
-                {{ csrf_field() }}
-                <fieldset>
-                    <legend>
-                        Add New Perpetrator
-                    </legend>
-                    <label for='perp_name'>Perpetrator Name</label>
-                    <input type='text' autocomplete='off' name='perp_name' id='perp_name'>
+    <div class='form-container'>
+        <form class='add-input' method='POST' action='{{ '/create' }}'>
+            {{ csrf_field() }}
+            <fieldset>
+                <legend>
+                    Add New Perpetrator
+                </legend>
+                <label for='perp_name'>Perpetrator Name</label>
+                <input type='text' autocomplete='off' name='perp_name' id='perp_name'>
 
-                    <label for='perp_dob'>Date of Birth:(format:YYYY-MM-DD)</label>
-                    <input type='text' autocomplete='off' name='date_of_birth' id='perp_dob'>
+                <label for='perp_arrest'>Date of Arrest:(format:YYYY-MM-DD)*</label>
+                <input type='text' autocomplete='off' name='arrest_date' id='perp_arrest'>
 
-                    <label for='perp_arrest'>Date of Arrest:(format:YYYY-MM-DD)</label>
-                    <input type='text' autocomplete='off' name='arrest_date' id='perp_arrest'>
+                <label for='description'>Perpetrator Details</label>
+                <textarea name='description' id='description' cols='50' rows='15'></textarea>
 
-                    <label for='perp_death'>Date of Death:(format:YYYY-MM-DD)</label>
-                    <input type='text' autocomplete='off' name='date_of_death' id='perp_death'>
+                <label for='perp_dob'>Date of Birth:(format:YYYY-MM-DD)</label>
+                <input type='text' autocomplete='off' name='date_of_birth' id='perp_dob'>
 
-                    <label for='description'>Perpetrator Details</label>
-                    <input type='text' autocomplete='off' name='description' id='description'>
-                </fieldset>
-                <button type='submit' class='btn btn-primary'>Save</button>
-            </form>
-        </div>
+                <label for='perp_death'>Date of Death:(format:YYYY-MM-DD)</label>
+                <input type='text' autocomplete='off' name='date_of_death' id='perp_death'>
+
+            </fieldset>
+            <button type='submit' class='btn btn-primary'>Save</button>
+        </form>
     </div>
-
-
-
 @endsection
