41f28f58cf0919072ccd6af13d1720da203b086e
Alex Caloggero <a.calog@protonmail.com>
2019-04-25 12:56:06 -0400

    Updated form and login styles

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,22 +1,22 @@
 @extends('layouts.master')
 
 @section('content')
-    <form method="POST" action="{{ route('login') }}">
+    <form method='POST' action='{{ route('login') }}'>
         @csrf
-        <label for="email">{{ __('E-Mail Address') }}</label>
-        <input id="email" type="email" name="email" value="{{ old('email') }}" required autofocus>
-        <label for="password">{{ __('Password') }}</label>
-        <input id="password" type="password" name="password" required>
-        <input class="form-check-input" type="checkbox" name="remember"
-               id="remember" {{ old('remember') ? 'checked' : '' }}>
-        <label class="form-check-label" for="remember">
+        <label for='email'>{{ __('E-Mail Address') }}</label>
+        <input id='email' class='minimal-text-input' type='email' name='email' value='{{ old('email') }}' required autofocus>
+        <label for='password'>{{ __('Password') }}</label>
+        <input id='password' type='password' name='password' required>
+        <input class='form-check-input' type='checkbox' name='remember'
+               id='remember' {{ old('remember') ? 'checked' : '' }}>
+        <label class='form-check-label' for='remember'>
             {{ __('Remember Me') }}
         </label>
-        <button type="submit" class="btn btn-login">
+        <button type='submit' class='btn btn-login'>
             {{ __('Login') }}
         </button>
         @if (Route::has('password.request'))
-            <a class="btn-link" href="{{ route('password.request') }}">
+            <a class='btn-link' href='{{ route('password.request') }}'>
                 {{ __('Forgot Your Password?') }}
             </a>
         @endif
--- resources/views/case/create.blade.php
+++ resources/views/case/create.blade.php
@@ -2,19 +2,16 @@
 
 @section('content')
     <div class='form-container'>
+        <h2>Case / Perpetrator Details</h2>
         <form class='add-input' method='POST' action='{{ '/create' }}'>
             {{ csrf_field() }}
-            <fieldset>
-                <legend>
-                    Add New Perpetrator
-                </legend>
-                <label for='perp_name'>Perpetrator Name</label>
+                <label for='perp_name'>Perpetrator Name*</label>
                 <input type='text' autocomplete='off' name='perp_name' id='perp_name'>
 
                 <label for='perp_arrest'>Date of Arrest:(format:YYYY-MM-DD)*</label>
                 <input type='text' autocomplete='off' name='arrest_date' id='perp_arrest'>
 
-                <label for='description'>Perpetrator Details</label>
+                <label for='description'>Perpetrator Details*</label>
                 <textarea name='description' id='description' cols='50' rows='15'></textarea>
 
                 <label for='perp_dob'>Date of Birth:(format:YYYY-MM-DD)</label>
@@ -22,8 +19,6 @@
 
                 <label for='perp_death'>Date of Death:(format:YYYY-MM-DD)</label>
                 <input type='text' autocomplete='off' name='date_of_death' id='perp_death'>
-
-            </fieldset>
             <button type='submit' class='btn btn-primary'>Save</button>
         </form>
     </div>
