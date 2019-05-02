a0d5317668888f015a1622c8c60dfda61945e878
Alex Caloggero <a.calog@protonmail.com>
2019-05-02 18:06:14 -0400

    Updated stylesheet and sass

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -46,10 +46,7 @@
 </footer>
 
 
-{{-- JS global to every page can be loaded here; jQuery included just as an example --}}
-<script src='{{ 'https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js' }}'
-        integrity='sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa'
-        crossorigin='anonymous'></script>
+
 
 {{-- JS specific to a given page/child view can be included via a stack --}}
 @stack('body')
