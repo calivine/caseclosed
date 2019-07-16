c4e396583fee8911d07681b08f2c0f168a2e2e9c
Alex Caloggero <a.calog@protonmail.com>
2019-07-15 20:41:24 -0400

    Updated image size on home-profile

--- resources/views/modules/home-profile.blade.php
+++ resources/views/modules/home-profile.blade.php
@@ -3,7 +3,7 @@
         {{ $victim->first_name }} {{ $victim->middle_name }} {{ $victim->last_name }}
     </p>
     @if($profileImage)
-        <img src='{{ $profileImage->url }}' alt='{{ $profileImage->caption }}'>
+        <img src='{{ $profileImage->url }}' alt='{{ $profileImage->caption }}' width='450px' height='600px'>
     @endif
     <p>
         {!! nl2br(e($victim->description)) !!}
