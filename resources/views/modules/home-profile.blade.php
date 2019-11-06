a3663734a46bb04fd5ffbd16c083badf593b5a21
Alex Caloggero <a.calog@protonmail.com>
2019-11-06 17:04:26 -0500

    Updated styles

--- resources/views/modules/home-profile.blade.php
+++ resources/views/modules/home-profile.blade.php
@@ -3,7 +3,10 @@
         {{ $victim->first_name }} {{ $victim->middle_name }} {{ $victim->last_name }}
     </p>
     @if($profileImage)
-        <img src='{{ $profileImage->url }}' alt='{{ $profileImage->caption }}' width='450px' height='600px'>
+        <img src='{{ $profileImage->url }}'
+             alt='{{ $profileImage->caption }}'
+             width='450px'
+             height='600px'>
     @endif
     <p>
         {!! nl2br(e($victim->description)) !!}
